fa7dd609c1b365fb2a30d38cb578f6b7601b5abf
anna clara sul <annaclaralourencosul@gmail.com>
2024-02-29 16:15:32 -0300

    netbeans
    criação do projeto no netbeans com a ferramenta Spring MVC.

---
+++ MeuDinheiro/src/main/java/com/senac/MeuDinheiro/MeuDinheiroApplication.java
@@ -0,0 +1,13 @@
+package com.senac.MeuDinheiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuDinheiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuDinheiroApplication.class, args);
+	}
+
+}
