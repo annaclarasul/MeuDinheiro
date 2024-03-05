78d155b5b89a7326734213c850f78e7d8e2eaa2c
anna clara sul <annaclaralourencosul@gmail.com>
2024-03-05 19:40:58 -0300

    usuario
    classe usuario controller

--- MeuDinheiro/src/main/java/com/senac/MeuDinheiro/controller/UsuarioService.java
+++ MeuDinheiro/src/main/java/com/senac/MeuDinheiro/controller/UsuarioService.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Usuario;
+package com.senac.MeuDinheiro.controller;
 
 /**
  *
  * @author ferna
  */
-public class controller {
+class UsuarioService {
     
 }
