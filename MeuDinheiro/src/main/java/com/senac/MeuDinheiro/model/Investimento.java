7ff87c09920d366d25c7cda665f87c387ede9b30
anna clara sul <annaclaralourencosul@gmail.com>
2024-03-05 17:59:46 -0300

    classes
    preenchimento da classe usuario e criaçao da classe usuario.controller

--- MeuDinheiro/src/main/java/com/senac/MeuDinheiro/model/Investimento.java
+++ MeuDinheiro/src/main/java/com/senac/MeuDinheiro/model/Investimento.java
@@ -1,13 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.senac.MeuDinheiro.model;
 
-/**
- *
- * @author ferna
- */
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+@EntityScan
 public class Investimento {
     
 }
--- MeuDinheiro/src/main/java/com/senac/MeuDinheiro/model/Usuario.java
+++ MeuDinheiro/src/main/java/com/senac/MeuDinheiro/model/Usuario.java
@@ -1,13 +1,37 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.senac.MeuDinheiro.model;
 
-/**
- *
- * @author ferna
- */
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+@EntityScan
 public class Usuario {
+    private String nome;
+    private String email;
+    private String senha;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    
     
 }
