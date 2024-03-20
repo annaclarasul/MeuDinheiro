c06c2dd88c7a9699de650249439a80af74a4a2da
anna clara sul <annaclaralourencosul@gmail.com>
2024-03-20 11:31:13 -0300

    Create login.java

---
+++ MeuDinheiro/src/main/java/com/senac/MeuDinheiro/model/login.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.senac.MeuDinheiro.model;
+
+public class login {
+
+    private String email;
+    private String senha;
+
+    public login(String email, String senha) {
+        this.email = email;
+        this.senha = senha;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
