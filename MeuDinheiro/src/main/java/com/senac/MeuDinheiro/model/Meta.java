34c0ad9a7af56f7d1e05ed551a8cf77b855c86fb
anna clara sul <annaclaralourencosul@gmail.com>
2024-03-20 11:31:10 -0300

    Create Meta.java

---
+++ MeuDinheiro/src/main/java/com/senac/MeuDinheiro/model/Meta.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.senac.MeuDinheiro.model;
+
+/**
+ *
+ * @author ferna
+ */
+class Meta {
+
+    void setUser(Usuario user) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    int getId() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
