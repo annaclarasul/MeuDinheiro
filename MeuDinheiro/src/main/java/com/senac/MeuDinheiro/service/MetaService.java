dc3a15b4f27db14936f7e004e8caf4cb08e0d2fe
anna clara sul <annaclaralourencosul@gmail.com>
2024-03-20 19:52:13 -0300

    Update MetaService.java

--- MeuDinheiro/src/main/java/com/senac/MeuDinheiro/service/MetaService.java
+++ MeuDinheiro/src/main/java/com/senac/MeuDinheiro/service/MetaService.java
@@ -4,40 +4,13 @@
  */
 package com.senac.MeuDinheiro.service;
 
-// src/main/java/com/senac/MeuDinheiro/service/MetaService.java
 
 import com.senac.MeuDinheiro.model.Meta;
-import com.senac.MeuDinheiro.model.MetaDriver;
 import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
-import { Injectable } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
-import { Meta } from './meta.entity';
-
-@Injectable()
-export class MetaService {
-  constructor(
-    @InjectRepository(Meta)
-    private metaRepository: Repository<Meta>,
-  ) {}
-
-  async findAll(): Promise<Meta[]> {
-    return this.metaRepository.find();
-  }
-
-  async findOne(id: number): Promise<Meta> {
-    return this.metaRepository.findOne(id);
-  }
-
-  async update(id: number, meta: Meta): Promise<Meta> {
-    await this.metaRepository.update(id, meta);
-    return this.findOne(id);
-  }
-
-  async delete(id: number): Promise<void> {
-    await this.metaRepository.delete(id);
-  }
+public interface MetaService {
+    List<Meta> readAll();
+    Meta create(Meta meta);
+    Meta update(Meta meta);
+    boolean delete(int id);
 }
