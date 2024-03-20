510e8ca8c9ef197c0dc9cd7ecc5b3de597ec7b11
anna clara sul <annaclaralourencosul@gmail.com>
2024-03-20 11:31:18 -0300

    Update Categoria.java

--- MeuDinheiro/src/main/java/com/senac/MeuDinheiro/model/Categoria.java
+++ MeuDinheiro/src/main/java/com/senac/MeuDinheiro/model/Categoria.java
@@ -4,10 +4,41 @@
  */
 package com.senac.MeuDinheiro.model;
 
-/**
- *
- * @author ferna
- */
 public class Categoria {
+    private Long id;
+    private String nome;
+    private String icone;
+    
+    public Categoria(Long id, String nome, String icone) {
+        this.id = id;
+        this.nome = nome;
+        this.icone = icone;
+    }
+    
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getIcone() {
+        return icone;
+    }
+
+    public void setIcone(String icone) {
+        this.icone = icone;
+    }
+    
     
 }
