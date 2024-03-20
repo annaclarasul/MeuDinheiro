2195ae2a404907596f030c95b748b681027adaa2
anna clara sul <annaclaralourencosul@gmail.com>
2024-03-20 11:31:30 -0300

    Update MeuDinheiroApplication.java

--- MeuDinheiro/src/main/java/com/senac/MeuDinheiro/MeuDinheiroApplication.java
+++ MeuDinheiro/src/main/java/com/senac/MeuDinheiro/MeuDinheiroApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MeuDinheiroApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MeuDinheiroApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MeuDinheiroApplication.class, args);
+    }
 
 }
