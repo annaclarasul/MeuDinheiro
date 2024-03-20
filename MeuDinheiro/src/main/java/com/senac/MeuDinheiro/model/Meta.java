d148735067be5b82fb1abc45a39bc90e254e5474
anna clara sul <annaclaralourencosul@gmail.com>
2024-03-20 14:48:21 -0300

    Update Meta.java

--- MeuDinheiro/src/main/java/com/senac/MeuDinheiro/model/Meta.java
+++ MeuDinheiro/src/main/java/com/senac/MeuDinheiro/model/Meta.java
@@ -4,18 +4,71 @@
  */
 package com.senac.MeuDinheiro.model;
 
-/**
- *
- * @author ferna
- */
-class Meta {
+// src/main/java/com/senac/MeuDinheiro/model/Meta.java
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Meta {
+    private int id;
+    private String nome;
+    private double valor;
+    private String tipo;
+    private String prazo;
+    @Autowired
+    private User user;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getPrazo() {
+        return prazo;
+    }
+
+    public void setPrazo(String prazo) {
+        this.prazo = prazo;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 
     void setUser(Usuario user) {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
-
-    int getId() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-    
 }
