f98d2b1ce599cf1895ddacaeb61f521d260dc068
anna clara sul <annaclaralourencosul@gmail.com>
2024-03-20 14:48:06 -0300

    Update ContaService.java

--- MeuDinheiro/src/main/java/com/senac/MeuDinheiro/service/ContaService.java
+++ MeuDinheiro/src/main/java/com/senac/MeuDinheiro/service/ContaService.java
@@ -4,10 +4,56 @@
  */
 package com.senac.MeuDinheiro.service;
 
-/**
- *
- * @author ferna
- */
+import com.senac.MeuDinheiro.model.Conta;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 public class ContaService {
-    
+
+    private List<Conta> contas;
+
+    public ContaService() {
+        contas = new ArrayList<>();
+    }
+
+    public List<Conta> getContas() {
+        return contas;
+    }
+
+    public void addConta(Conta conta) {
+        contas.add(conta);
+    }
+
+    public Conta getContaById(int id) {
+        for (Conta conta : contas) {
+            if (conta.getId() == id) {
+                return conta;
+            }
+        }
+        return null;
+    }
+
+    public void updateConta(Conta conta) {
+        for (int i = 0; i < contas.size(); i++) {
+            if (contas.get(i).getId() == conta.getId()) {
+                contas.set(i, conta);
+                break;
+            }
+        }
+    }
+
+    public void deleteConta(int id) {
+        if (contas != null) {
+            Iterator<Conta> iterator = contas.iterator();
+            while (iterator.hasNext()) {
+                Conta conta = iterator.next();
+                if (conta.id == id) {
+                    iterator.remove();
+                    break;
+                }
+            }
+        }
+    }
+
 }
