b4046a3bbc33ed5b4d21914d481bb5d3f3f39416
anna clara sul <annaclaralourencosul@gmail.com>
2024-03-20 14:48:24 -0300

    Update Conta.java

--- MeuDinheiro/src/main/java/com/senac/MeuDinheiro/model/Conta.java
+++ MeuDinheiro/src/main/java/com/senac/MeuDinheiro/model/Conta.java
@@ -4,10 +4,64 @@
  */
 package com.senac.MeuDinheiro.model;
 
-/**
- *
- * @author ferna
- */
 public class Conta {
-    
+     public int id;
+    private String nome;
+    private String tipo;
+    private double saldo;
+    private Usuario usuario;
+
+    // construtor
+    public Conta(int id, String nome, String tipo, double saldo, Usuario usuario) {
+        this.id = id;
+        this.nome = nome;
+        this.tipo = tipo;
+        this.saldo = saldo;
+        this.usuario = usuario;
+    }
+
+    public Conta() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    // getters and setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
 }
