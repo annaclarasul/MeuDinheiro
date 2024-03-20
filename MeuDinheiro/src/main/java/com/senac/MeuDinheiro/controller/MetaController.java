04a3509e77a7dc6a595fece136091bdaa78503e2
anna clara sul <annaclaralourencosul@gmail.com>
2024-03-20 14:48:28 -0300

    Update MetaController.java

--- MeuDinheiro/src/main/java/com/senac/MeuDinheiro/controller/MetaController.java
+++ MeuDinheiro/src/main/java/com/senac/MeuDinheiro/controller/MetaController.java
@@ -4,10 +4,39 @@
  */
 package com.senac.MeuDinheiro.controller;
 
-/**
- *
- * @author ferna
- */
+// src/main/java/com/senac/MeuDinheiro/controller/MetaController.java
+
+import com.senac.MeuDinheiro.model.Meta;
+import com.senac.MeuDinheiro.service.MetaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/metas")
 public class MetaController {
-    
+    @Autowired
+    private MetaService metaService;
+
+    @GetMapping
+    public List<Meta> getAllMetas() {
+        return metaService.readAll();
+    }
+
+    @PostMapping
+    public Meta createMeta(@RequestBody Meta meta) {
+        return metaService.create(meta);
+    }
+
+    @PutMapping("/{id}")
+    public Meta updateMeta(@PathVariable int id, @RequestBody Meta meta) {
+        meta.setId(id);
+        return metaService.update(meta);
+    }
+
+    @DeleteMapping("/{id}")
+    public boolean deleteMeta(@PathVariable int id) {
+        return metaService.delete(id);
+    }
 }
